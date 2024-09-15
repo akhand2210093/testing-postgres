c560b6fc496995e3562c5096116cb4fa477e500f
akhand2210093 <akhandpratapsingh11c@gmail.com>
2024-09-15 16:45:37 +0530

    removed static files

--- spocc/settings.py
+++ spocc/settings.py
@@ -177,10 +177,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# import os
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 
